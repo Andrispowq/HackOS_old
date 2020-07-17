f87c921ec16b42f573f1b116e214ec9e74092002
Andrispowq <akmeczo@gmail.com>
2020-07-17 16:50:11 +0200

    Removed random comments in paging.c from debugging and removed object files

--- kernel/cpu/paging/paging.c
+++ kernel/cpu/paging/paging.c
@@ -114,35 +114,8 @@ void free_frame(page_t* page)
     PAGING CODE
 */
 
-//uint32_t page_directory[1024] __attribute__((aligned(4096)));
-//uint32_t first_page_table[1024] __attribute__((aligned(4096)));
-
 void initialise_paging()
 {
-    //set each entry to not present
-    /*int i;
-    for(i = 0; i < 1024; i++)
-    {
-        // This sets the following flags to the pages:
-        //   Supervisor: Only kernel-mode can access them
-        //   Write Enabled: It can be both read from and written to
-        //   Not Present: The page table is not present
-        page_directory[i] = 0x00000002;
-    }
-    
-    //we will fill all 1024 entries in the table, mapping 4 megabytes
-    for(i = 0; i < 1024; i++)
-    {
-        // As the address is page aligned, it will always leave 12 bits zeroed.
-        // Those bits are used by the attributes ;)
-        first_page_table[i] = (i * 0x1000) | 3; // attributes: supervisor level, read/write, present.
-    }
-
-    // attributes: supervisor level, read/write, present
-    page_directory[0] = ((unsigned int)first_page_table) | 3;
-
-    printf("Page directory address: %x\n", page_directory);*/
-
     // The size of physical memory. For the moment we
     // assume it is 16MB big.
     uint32_t mem_end_page = 0x1000000;
@@ -188,7 +161,7 @@ void initialise_paging()
 void switch_page_directory(page_directory_t* dir)
 {
     current_directory = dir;
-    asm volatile("mov %0, %%cr3" :: "r"(dir->tablesPhysical)/*(page_directory)*/);
+    asm volatile("mov %0, %%cr3" :: "r"(dir->tablesPhysical));
     uint32_t cr0;
     asm volatile("mov %%cr0, %0" : "=r"(cr0));
     cr0 |= 0x80000000; // Enable paging!
